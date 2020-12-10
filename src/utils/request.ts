8b421935e3a992065321a37592b0af493649cba8
苏祥凤 <suxiangfeng@ebaochina.cn>
2020-12-10 10:48:51 +0800

    fix:添加prefix

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,9 +2,12 @@
  * request 网络请求工具
  * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
-import { history } from 'umi';
+// import { history } from 'umi';
 import { extend } from 'umi-request';
 import { notification } from 'antd';
+
+const { NODE_ENV } = process.env;
+const { protocol } = window.location;
 
 const codeMessage = {
   200: '服务器成功返回请求的数据。',
@@ -65,9 +68,16 @@ const errorHandler = (error: { response: Response }): Response => {
 /**
  * 配置request请求时的默认参数
  */
+const prefix =
+  NODE_ENV === 'development'
+    ? '/api'
+    : // @ts-ignore
+      `${protocol}//${API_DOMAIN}`;
+
 const request = extend({
+  prefix,
   errorHandler, // 默认错误处理
-  credentials: 'include', // 默认请求是否带上cookie
+  credentials: 'same-origin', // 默认请求是否带上cookie
 });
 
 request.use(async (ctx, next) => {
@@ -81,7 +91,7 @@ request.use(async (ctx, next) => {
 
   if (res?.code !== 200) {
     notification.error({
-      description: res?.msg ?? '出错了',
+      description: res?.message ?? '出错了',
       message: '错误',
     });
   }
